efd1dc5ecb2491a8042829fbb5860cf118573000
ScriptedAlchemy <zjackson@lululemon.com>
2020-03-05 11:17:45 -0800

    attempt to flag errors

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -364,7 +364,8 @@ export default async function build(dir: string, conf = null): Promise<void> {
   if (buildSpinner) {
     buildSpinner.stopAndPersist()
   }
-  console.log()
+  console.log('There were errors!')
+  console.log(result)
   console.log(JSON.stringify(result, null, 2))
   // result = formatWebpackMessages(result)
 
