bce608caabce5b587388c964cd0147ce85e32001
zackjackson <zackary.l.jackson@gmail.com>
2020-03-14 14:28:30 -0700

    make resolve-request.ts backward compatible

--- packages/next/lib/resolve-request.ts
+++ packages/next/lib/resolve-request.ts
@@ -1,5 +1,5 @@
-// import resolve from 'next/dist/compiled/resolve/index.js'
-// import path from 'path'
+import resolve from 'next/dist/compiled/resolve/index.js'
+import path from 'path'
 const webpack5Experiential = parseInt(require('webpack').version) === 5
 
 export function resolveRequest(req: string, issuer: string) {
@@ -13,14 +13,14 @@ export function resolveRequest(req: string, issuer: string) {
   // The `resolve` package is prebuilt through ncc, which prevents
   // PnP from being able to inject itself into it. To circumvent
   // this, we simply use PnP directly when available.
-  // if (process.versions.pnp) {
-  //   const { resolveRequest } = require(`pnpapi`)
-  //   return resolveRequest(req, issuer, { considerBuiltins: false })
-  // } else {
-  //   const basedir =
-  //     issuer.endsWith(path.posix.sep) || issuer.endsWith(path.win32.sep)
-  //       ? issuer
-  //       : path.dirname(issuer)
-  //   return resolve.sync(req, { basedir })
-  // }
+  if (process.versions.pnp) {
+    const { resolveRequest } = require(`pnpapi`)
+    return resolveRequest(req, issuer, { considerBuiltins: false })
+  } else {
+    const basedir =
+      issuer.endsWith(path.posix.sep) || issuer.endsWith(path.win32.sep)
+        ? issuer
+        : path.dirname(issuer)
+    return resolve.sync(req, { basedir })
+  }
 }
