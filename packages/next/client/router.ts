1343058b9d566bd95672611f486aa951b3790a01
zackjackson <zackary.l.jackson@gmail.com>
2020-03-26 19:22:52 -0700

    fix: dirty patch for missing router context

--- packages/next/client/router.ts
+++ packages/next/client/router.ts
@@ -123,6 +123,9 @@ export function useRouter() {
   if (process.browser) {
     return React.useContext(RouterContext)
   }
+  if(React.useContext(RouterContext)) {
+    return React.useContext(RouterContext)
+  }
   return global.router
 }
 
